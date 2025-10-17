6e60793f01da776784383435dd3e4a64e81109f8
mateo1711 <99088704+mateo1711@users.noreply.github.com>
2025-10-17 18:16:43 +0200

    Add files via upload

---
+++ commands/message/nuke.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
+client.on('ready', () => {
+    console.log('Codelyon is online!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'clear') {
+        client.commands.get('clear').execute(message, args);
+    }
+});
+
