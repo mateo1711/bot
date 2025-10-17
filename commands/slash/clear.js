9ee60929e4af5f9459b95668efa428ce465b69fe
mateo1711 <99088704+mateo1711@users.noreply.github.com>
2025-10-17 18:09:55 +0200

    Update clear.js

--- commands/slash/clear.js
+++ commands/slash/clear.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: 'clear',
-    description: 'Clear messages!",
+    name: 'clean',
+    description: 'Clean messages!",
     async execute(message, args)  {
-        if(!args[0]) return message.reply("please enter the amount of messages that you want to clear!");
+        if(!args[0]) return message.reply("please enter the amount of messages that you want to clean!");
         if(isNaN(args[0])) return message.reply("please enter a real number!");
         if(args[0] > 100) return message.reply("You cannot delete more than 100 messages!");
         if(args[0] < 1) return message.reply("You must delete at least one message!");
@@ -12,3 +12,4 @@ module.exports = {
         });
     }
 };
+
